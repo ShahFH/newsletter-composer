5db26d1c55aa52068665f216acfd7a29ceaacd1b
ShahFH <shahfaisal.swe@gmail.com>
2025-09-15 15:28:20 +0430

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Suspense } from "react"
@@ -22,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body >
         <Suspense fallback={null}>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
             <header className="border-b ">
